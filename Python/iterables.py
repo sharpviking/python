dcebd3261a962c0e4ef44a1f462314d33def1161
sharpviking <110773486+sharpviking@users.noreply.github.com>
2023-03-31 00:53:32 +0530

    Add files via upload

---
+++ Python/iterables.py
@@ -0,0 +1,6 @@
+for x in ("python"):
+    print(x)
+
+
+for x in [1, 2, 3, 4, 5]:
+    print(x)
---
+++ Python/string_methods.py
@@ -0,0 +1,10 @@
+course = "python programming"
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.strip())  # to remove white spaces in string
+print(course.lstrip())  # to remove lsft white spaces in string
+print(course.rstrip())  # to remove right white spaces in string
+print(course.find("pro"))  # returns index
+print(course.replace("p", "a"))
+print("pro" in course)  # returns "Boolean"

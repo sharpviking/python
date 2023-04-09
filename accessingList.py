b557ffe0d4068ee0bf66500ed117112fbfab50cd
Abhishek Anand <anandabhishek3607@gmail.com>
2023-04-10 00:59:00 +0530

    9th April commit

---
+++ accessingList.py
@@ -0,0 +1,6 @@
+letters = ["a", "b", "c", "d", "e"]
+print(letters[-1])
+
+numbers = list(range(20))
+print(numbers[::2])
+print(numbers[::-1])
---
+++ lists.py
@@ -0,0 +1,8 @@
+letters = ["a", "b", "c", "d", "e"]
+matrix = [[0, 1], [26, 11]]
+# zero = [0]*100
+zero = [0]*5
+combined = zero+letters
+number = list(range(20))
+chars = list('Hello World')
+print(len(chars))

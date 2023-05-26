f5beeb99f393403574290bcbb93d405fab76b030
Abhishek Anand <anandabhishek3607@gmail.com>
2023-05-26 18:41:30 +0530

    26th May commit

--- flask/create.py
+++
@@ -1,8 +0,0 @@
-from product import products
-
-
-def create_product(self, name, price):
-    product_id = len(self.products) + 1
-    new_product = Product(product_id, name, price)
-    self.products.append(new_product)
-    return new_product.id
---
+++ tuples.py
@@ -0,0 +1,9 @@
+point = (91, 2, 53)
+print(point[0:2])
+x, y, x = point
+if 10 in point:
+    print("exits")
+
+
+pointer = tuple("hEllo John")
+print(pointer)

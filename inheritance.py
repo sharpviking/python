49a601fa5ce88e55e1ae7833b8bc4132cc017455
Abhishek Anand <anandabhishek3607@gmail.com>
2023-08-17 13:00:01 +0530

    inheritance

---
+++ inheritance.py
@@ -0,0 +1,19 @@
+class Animal:
+
+    def __init__(self):
+        self.age = 1
+
+    def eat(Self):
+        print("eat")
+
+
+class Mammal(Animal):
+
+    def walk(self):
+        print("walk")
+
+
+class Fish(Animal):
+
+    def swim(self):
+        print("swim")

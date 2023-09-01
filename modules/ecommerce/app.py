c804f6c948c0e51e16aa0693acfac6a2052ccc0a
Abhishek Anand <anandabhishek3607@gmail.com>
2023-09-02 00:56:40 +0530

    modules

---
+++ modules/ecommerce/app.py
@@ -0,0 +1,13 @@
+# from modules.ecommerce.sales import calc_shipping, calc_tax
+
+
+# import modules.ecommerce.app as app
+
+# app.calc_shipping()
+
+
+# calc_shipping()
+# calc_tax()
+
+
+from ecommerce.sales import calc_tax
